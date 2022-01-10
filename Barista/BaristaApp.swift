470f3ac0a42783e4e51f0783d07ca50f827650a4
Rogier van den Brink <r>
2022-01-10 09:27:22 +0100

    Initial Commit

---
+++ Barista/BaristaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaristaApp.swift
+//  Barista
+//
+//  Created by Rogier van den Brink on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BaristaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
